64fb739d7e707d77753df8bd32fb8d15c0be6a99
yangyymm <yangyimingsd@163.com>
2024-10-19 11:52:54 +0800

    水仙花数

---
+++ CODE/C0105.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//Ë®ÏÉ»¨Êý
+	int count = 0;
+	for (int i = 100; i <= 999; i++)
+	{
+		int tmp = i, j = i;
+		int a = j % 10;
+		j /= 10;
+		int b = j % 10;
+		j /= 10;
+		int c = j;
+		if (tmp == a * a * a + b * b * b + c * c * c) {
+			printf("%d\n", tmp);
+			count++;
+		}
+	}
+	printf("count: %d\n", count);
+	return 0;
+}
