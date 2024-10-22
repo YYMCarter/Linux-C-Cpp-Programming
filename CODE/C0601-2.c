85e32d9615d577b6f6d6a18e3bff95bdea492c84
yangyymm <yangyimingsd@163.com>
2024-10-22 13:02:41 +0800

    十进制转换成二进制

---
+++ CODE/C0601-2.c
@@ -0,0 +1,19 @@
+/*十进制转换成二进制*/
+#include <stdio.h>
+
+void ten_to_two(int number)
+{
+	if (number <= 0) {
+		return;
+	}
+	ten_to_two(number / 2);
+	printf("%d ", number % 2);
+}
+
+int main(void)
+{
+	int number;
+	scanf("%d", &number);
+	ten_to_two(number);
+	return 0;
+}
